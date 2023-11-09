111a75842b4572f8d835f83ecc850dfa2c6bb237
Tymek Gryszkalis <lalarsek@gmail.com>
2023-11-09 16:10:45 +0100

    upload

---
+++ controller.py
@@ -0,0 +1,8 @@
+def getClientById(id):
+    pass
+
+def getProductById(id):
+    pass
+
+def getOrderById(id):
+    pass
---
+++ ui.py
@@ -0,0 +1,4 @@
+import controller as con
+
+while True:
+    inp = input("> ").split(" ")
