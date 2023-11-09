fffe5fa8162c698562874e6b6c7d6b99f2334f8d
Tymek Gryszkalis <lalarsek@gmail.com>
2023-11-09 23:18:21 +0100

    added getters and init

--- controller.py
+++ controller.py
@@ -1,8 +1,24 @@
+import db
+
+def dbInit():
+    db.init()
+
 def getClientById(id):
-    pass
+    return db.select("client", id)
 
 def getProductById(id):
-    pass
+    return db.select("product", id)
 
 def getOrderById(id):
-    pass
+    return db.select("product", id)
+
+def getAllClients():
+    return db.select("client", all = True)
+
+def getAllProducts():
+    return db.select("product", all = True)
+
+def getAllOrders():
+    return db.select("order", all = True)
+
+
